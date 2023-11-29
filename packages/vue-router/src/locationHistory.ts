600a724391dd0065998c069f306cdaefddb397ef
har0ke <har0ke@tutanota.com>
2023-11-29 21:48:22 +0100

    Log some data.

--- packages/vue-router/src/locationHistory.ts
+++ packages/vue-router/src/locationHistory.ts
@@ -254,7 +254,34 @@ export const createLocationHistory = () => {
     return undefined;
   };
 
+  const logInfo = (prefix: string) => {
+    console.log("HISTORY START", prefix, window.location.pathname, window.history.state.position)
+    for (let i = locationHistory.length - 2; i >= 0; i--) {
+        console.log(
+            "locationHistory".padStart(15),
+            locationHistory[i].tab.padStart(15),
+            String(locationHistory[i].position).padStart(5),
+            locationHistory[i].pathname.padStart(30),
+            locationHistory[i].lastPathname.padStart(30),
+            locationHistory[i].routerAction.padStart(10));
+    }
+    Object.keys(tabsHistory).forEach((key) => {
+
+        for (let i = tabsHistory[key].length - 2; i >= 0; i--) {
+            console.log(
+                key.padStart(15),
+                tabsHistory[key][i].tab.padStart(15),
+                String(tabsHistory[key][i].position).padStart(5),
+                tabsHistory[key][i].pathname.padStart(30),
+                tabsHistory[key][i].lastPathname.padStart(30),
+                tabsHistory[key][i].routerAction.padStart(10));
+        }
+    })
+    console.log("end", prefix)
+
+}
   return {
+    logInfo,
     current,
     size,
     last,
